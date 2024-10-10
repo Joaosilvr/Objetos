a4938db9beab995914fafcb6deaeb279312ad52f
joaosilvr <joaovitor.dasilvaramos@gmail.com>
2024-10-09 21:34:29 -0300

    adicionando mais exercicios

---
+++ objetos2.js
@@ -0,0 +1,9 @@
+// exercicio 2
+
+const pessoa = {
+    nome: 'Maria',
+    idade: 20,
+    generoMusical:'rock'
+}
+console.log(`O nome da pessoa é ${pessoa.nome}, ela tem ${pessoa.idade} anos e gosta muito de ${pessoa.generoMusical}.`);
+
---
+++ objetos3.js
@@ -0,0 +1,27 @@
+// exercicio 3
+
+const filmes = {
+    direcao: 'disney',
+    nome: 'Deadpool e wolverine',
+    ano: 2024,
+    elenco: ['hugh Jackman','Ryan Reynolds','Chris Evans'],
+    assistiu: true,
+}
+console.log(filmes.direcao);
+console.log(filmes.nome);
+console.log(filmes.ano);
+console.log(filmes['elenco']);
+console.log(filmes['assistiu']);
+
+filmes.personagens = ['Wolverine','Deadpool','Tocha humana']
+
+console.log(filmes.elenco[0],'=',filmes.personagens[0]);
+console.log(filmes.elenco[1],'=',filmes.personagens[1]);
+console.log(filmes.elenco[2],'=',filmes.personagens[2]);
+
+
+filmes.elenco.splice(0,1,'xuxa') // fiz 2 só para fixar
+filmes.elenco[0] = 'xuxa1' // só para fixar
+console.log(filmes.elenco);
+
+console.log(filmes);
